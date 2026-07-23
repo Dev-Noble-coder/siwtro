4a1b4f8b23ac89486a911d0f188c4180b8469572
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2026-07-23 22:14:51 +0100

    Changes to the site

--- src/pages/StudentPortal.tsx
+++ src/pages/StudentPortal.tsx
@@ -118,10 +118,43 @@ export default function StudentPortal() {
                 <label className="block text-sm font-medium text-gray-700 mb-1">State of Placement</label>
                 <select required name="state" value={sharedDetails.state} onChange={handleSharedChange} className="sharp-input w-full">
                   <option value="">Select State</option>
-                  <option value="Lagos">Lagos</option>
-                  <option value="Oyo">Oyo</option>
+                  <option value="Abia">Abia</option>
+                  <option value="Adamawa">Adamawa</option>
+                  <option value="Akwa Ibom">Akwa Ibom</option>
+                  <option value="Anambra">Anambra</option>
+                  <option value="Bauchi">Bauchi</option>
+                  <option value="Bayelsa">Bayelsa</option>
+                  <option value="Benue">Benue</option>
+                  <option value="Borno">Borno</option>
+                  <option value="Cross River">Cross River</option>
+                  <option value="Delta">Delta</option>
+                  <option value="Ebonyi">Ebonyi</option>
+                  <option value="Edo">Edo</option>
+                  <option value="Ekiti">Ekiti</option>
+                  <option value="Enugu">Enugu</option>
                   <option value="FCT">FCT</option>
-                  <option value="Other">Other</option>
+                  <option value="Gombe">Gombe</option>
+                  <option value="Imo">Imo</option>
+                  <option value="Jigawa">Jigawa</option>
+                  <option value="Kaduna">Kaduna</option>
+                  <option value="Kano">Kano</option>
+                  <option value="Katsina">Katsina</option>
+                  <option value="Kebbi">Kebbi</option>
+                  <option value="Kogi">Kogi</option>
+                  <option value="Kwara">Kwara</option>
+                  <option value="Lagos">Lagos</option>
+                  <option value="Nasarawa">Nasarawa</option>
+                  <option value="Niger">Niger</option>
+                  <option value="Ogun">Ogun</option>
+                  <option value="Ondo">Ondo</option>
+                  <option value="Osun">Osun</option>
+                  <option value="Oyo">Oyo</option>
+                  <option value="Plateau">Plateau</option>
+                  <option value="Rivers">Rivers</option>
+                  <option value="Sokoto">Sokoto</option>
+                  <option value="Taraba">Taraba</option>
+                  <option value="Yobe">Yobe</option>
+                  <option value="Zamfara">Zamfara</option>
                 </select>
               </div>
               <div>
